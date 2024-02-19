4aeeb7ece555e55c0d444a71a0322165a3f09ce3
sun33003 <127114580+sun33003@users.noreply.github.com>
2024-02-19 20:37:40 +0800

    Create diy-part0.sh

---
+++ diy-part0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 添加源
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 应用过滤OpenAppFilter 【luci oaf app】
+# 和加速模块（Turbo ACC)有冲突，需要关闭Turboo ACC后使用
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter 
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
