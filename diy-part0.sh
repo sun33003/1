18dc674c861a1d84753843032e44f54cf4468491
sun33003 <127114580+sun33003@users.noreply.github.com>
2024-12-13 20:43:45 +0800

    Update diy-part0.sh

--- diy-part0.sh
+++ diy-part0.sh
@@ -14,7 +14,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 添加源
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # 应用过滤OpenAppFilter 【luci oaf app】
